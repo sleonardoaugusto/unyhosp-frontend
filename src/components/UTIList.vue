d35d6a171e3d3bff453ec1f08302484bd9427c77
Leonardo <sleonardoaugusto@gmail.com>
2019-12-09 15:36:54 -0300

    UTI Cards added

--- src/components/UTIList.vue
+++ src/components/UTIList.vue
@@ -1,10 +1,26 @@
 <template>
-  <div></div>
+  <div class="col-md-12 d-flex justify-content-center flex-wrap">
+    <div
+      v-for="(u, i) in utis"
+      :key="i"
+      class="card col-md-5"
+    >
+      <p class="card__name">{{ u.name }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import utis from '@/mocks/utis'
+
 export default {
-  name: 'UTIList'
+  name: 'UTIList',
+  data: () => ({
+    utis: []
+  }),
+  created() {
+    this.utis = utis.data
+  }
 }
 </script>
 
--- src/views/UTI.vue
+++ src/views/UTI.vue
@@ -1,10 +1,24 @@
 <template>
-  <div></div>
+  <div class="home col-md-12">
+    <div class="row">
+      <div class="col-md-12 d-flex justify-content-center">
+        <h1>UTIs</h1>
+      </div>
+    </div>
+    <div class="row">
+      <u-t-i-list/>
+    </div>
+  </div>
 </template>
 
 <script>
+import UTIList from '@/components/UTIList'
+
 export default {
-  name: 'UTI'
+  name: 'UTI',
+  components: {
+    UTIList
+  }
 }
 </script>
 

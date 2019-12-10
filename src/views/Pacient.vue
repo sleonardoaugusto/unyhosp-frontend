5c5d4ba3d6f5cc60d25dee9b212e6c50f59dbd85
Leonardo <sleonardoaugusto@gmail.com>
2019-12-10 17:23:41 -0300

    Pacient page form added

--- src/views/Pacient.vue
+++ src/views/Pacient.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>Pacient Detail</div>
+  <div class="col-md-12">
+    <div class="row d-flex justify-content-center">
+      <h1>Paciente</h1>
+    </div>
+    <div class="row d-flex justify-content-center">
+      <div class="row">
+        <label for="PacientName">Nome:</label>
+        <input type="text" id="PacientName" v-model="attendance.pacient.name">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import attendance from '@/mocks/attendance'
+
 export default {
-  name: 'Pacient'
+  name: 'Pacient',
+  data: () => ({
+    attendance: {}
+  }),
+  created() {
+    this.attendance = attendance.data
+  }
 }
 </script>
 

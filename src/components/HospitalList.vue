7ae61d07c422cb288e471f3526cbb5f78372b426
Leonardo Augusto <sleonardoaugusto@gmail.com>
2019-12-19 02:13:34 -0300

    Style and hospital list adjustments

--- src/components/HospitalList.vue
+++ src/components/HospitalList.vue
@@ -3,10 +3,16 @@
     <router-link
       v-for="(h, i) in hospitals"
       :key="i"
-      class="card col-md-5"
+      class="card col-md-3"
       :to="{name: 'hospital-utis', params: {hospitalId: h.id}}"
     >
-      <p class="card__name">{{ h.name }}</p>
+      <div class="row mb-5">
+        <p class="card__name">{{ h.name }}</p>
+      </div>
+      <div class="row justify-content-between w-100">
+        <span class="card__action type-blue">Novo atendimento</span>
+        <span class="card__action type-yellow">Editar</span>
+      </div>
     </router-link>
   </div>
 </template>

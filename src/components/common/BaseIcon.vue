0ddf5b2fc6e97b643cce47031705fbbbf15b794b
Leonardo Augusto <sleonardoaugusto@gmail.com>
2019-12-20 20:21:26 -0300

    Icon component added

---
+++ src/components/common/BaseIcon.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="icon-wrapper">
+    <svg class="icon" :width="width" :height="height">
+      <use v-bind="{'xlink:href':'/feather-sprite.svg#'+name}"></use>
+    </svg>
+    <slot name="description"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    width: {
+      type: [Number, String],
+      default: 16
+    },
+    height: {
+      type: [Number, String],
+      default: 16
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .icon-wrapper {
+    display: inline-flex;
+    align-items: center;
+    color: #FFF;
+    font-size: 1rem;
+    font-weight: 600;
+  }
+
+  .icon {
+    stroke: currentColor;
+    stroke-width: 2;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    fill: none;
+    margin-right: 6px;
+  }
+</style>
--- src/components/notification/Notification.vue
+++ src/components/notification/Notification.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="notification-bar">
     <p class="notification-bar__message">
-      <span class="notification-bar__close" @click="close">x</span>
+      <!--      <span class="notification-bar__close" @click="close">x</span>-->
+      <BaseIcon name="users">
+        <span slot="description"></span>
+      </BaseIcon>
       {{ notification.message }}
     </p>
   </div>
 </template>
 
 <script>
+import BaseIcon from '@/components/common/BaseIcon'
+
 export default {
   name: 'Notification',
+  components: {
+    BaseIcon
+  },
   props: {
     notification: {
       type: Object,

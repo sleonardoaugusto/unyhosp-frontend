76565461f9e25f33e2a01d26c19bdb676eec1dde
Leonardo Augusto <sleonardoaugusto@gmail.com>
2019-12-26 03:37:54 -0300

    Added tests to pacient register form behavior and methods

--- src/components/pacient/Register.vue
+++ src/components/pacient/Register.vue
@@ -4,29 +4,30 @@
       <h2 class="type-h4 type-blue-smoke">Cadastrar Paciente</h2>
     </div>
     <form @submit.prevent="submit" class="col-md-6">
-      <div class="form-group" :class="{ 'form-group--error': $v.data.name.$error }">
+      <div class="form-group" :class="{ 'form-group--error': $v.pacient.name.$error }">
         <label class="form__label" for="PacientName">Nome:</label>
-        <input class="form__input" type="text" id="PacientName" v-model.trim.lazy="$v.data.name.$model">
-        <div v-if="!$v.data.name.required" class="error--message">Campo obrigatório</div>
+        <input class="form__input" type="text" id="PacientName" v-model.trim.lazy="$v.pacient.name.$model">
+        <div v-if="!$v.pacient.name.required" class="error--message">Campo obrigatório</div>
       </div>
-      <div class="form-group" :class="{ 'form-group--error': $v.data.document_id.$error }">
+      <div class="form-group" :class="{ 'form-group--error': $v.pacient.document_id.$error }">
         <label class="form__label" for="DocumentId">CPF:</label>
         <the-mask class="form__input" type="text" id="DocumentId" mask="###.###.###-##"
-                  v-model.trim.lazy="$v.data.document_id.$model"/>
-        <div v-if="!$v.data.document_id.required" class="error--message">Campo obrigatório</div>
-        <div v-if="!$v.data.document_id.minLength" class="error--message">CPF inválido</div>
+                  v-model.trim.lazy="$v.pacient.document_id.$model"/>
+        <div v-if="!$v.pacient.document_id.required" class="error--message">Campo obrigatório</div>
+        <div v-if="!$v.pacient.document_id.minLength" class="error--message">CPF inválido</div>
       </div>
-      <div class="form-group" :class="{ 'form-group--error': $v.data.email.$error }">
+      <div class="form-group" :class="{ 'form-group--error': $v.pacient.email.$error }">
         <label class="form__label" for="Email">Email:</label>
-        <input class="form__input" type="text" id="Email" v-model.trim.lazy="$v.data.email.$model">
-        <div v-if="!$v.data.email.required" class="error--message">Campo obrigatório</div>
-        <div v-if="!$v.data.email.email" class="error--message">Email inválido</div>
+        <input class="form__input" type="text" id="Email" v-model.trim.lazy="$v.pacient.email.$model">
+        <div v-if="!$v.pacient.email.required" class="error--message">Campo obrigatório</div>
+        <div v-if="!$v.pacient.email.email" class="error--message">Email inválido</div>
       </div>
-      <div class="form-group" :class="{ 'form-group--error': $v.data.date_of_birth.$error }">
+      <div class="form-group" :class="{ 'form-group--error': $v.pacient.date_of_birth.$error }">
         <label class="form__label" for="DateOfBirth">Data de Nascimento:</label>
-        <input class="form__input" type="tel" id="DateOfBirth" v-mask="'##/##/####'"
-               v-model.trim.lazy="$v.data.date_of_birth.$model">
-        <div v-if="!$v.data.date_of_birth.required" class="error--message">Campo obrigatório</div>
+        <input class="form__input" type="text" id="DateOfBirth" v-mask="'##/##/####'"
+               v-model.trim.lazy="$v.pacient.date_of_birth.$model">
+        <div v-if="!$v.pacient.date_of_birth.required" class="error--message date">Campo obrigatório</div>
+        <div v-if="!$v.pacient.date_of_birth.minLength" class="error--message date">Data inválida</div>
       </div>
       <button class="button button-grey" type="submit">CADASTRAR</button>
     </form>
@@ -41,27 +42,26 @@ import { required, email, minLength } from 'vuelidate/lib/validators'
 export default {
   name: 'Register',
   data: () => ({
-    data: {
+    pacient: {
       name: '',
       document_id: '',
       email: '',
       date_of_birth: ''
-    },
-    submitStatus: null
+    }
   }),
   validations: {
-    data: {
+    pacient: {
       name: { required },
       email: { required, email },
       document_id: { required, minLength: minLength(11) },
-      date_of_birth: { required }
+      date_of_birth: { required, minLength: minLength(10) }
     }
   },
   methods: {
     submit() {
       this.$v.$touch()
       if (!this.$v.$invalid) {
-        PacientService.post(this.data)
+        PacientService.post(this.pacient)
           .then(() => {
             const payload = { message: 'Cadastro realizado!', type: 'success' }
             this.add(payload)
--- src/components/pacient/__tests__/Register.spec.js
+++ src/components/pacient/__tests__/Register.spec.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import Register from '@/components/pacient/Register'
 import VueTheMask from 'vue-the-mask'
 import Vuelidate from 'vuelidate'
+import PacientService from '@/services/PacientService'
+import pacient from '@/components/pacient/__tests__/__mocks__/pacient'
 
 const localVue = createLocalVue()
 localVue.use(VueRouter)
@@ -22,10 +24,52 @@ describe('Pacient/Register.vue', () => {
     expect(wrapper.html()).toMatchSnapshot()
   })
 
-  it('click button must call submit method', () => {
+  it('click button should call submit method', () => {
     const submit = jest.fn()
     wrapper.setMethods({ submit })
     wrapper.find('form').trigger('submit')
     expect(submit).toHaveBeenCalled()
+  })
+
+  it('mandatory form fields', () => {
+    const errors = wrapper.findAll('.error--message')
+    expect(errors.at(0).text()).toBe('Campo obrigatório')
+    expect(errors.at(1).text()).toBe('Campo obrigatório')
+    expect(errors.at(2).text()).toBe('Campo obrigatório')
+    expect(errors.at(3).text()).toBe('Campo obrigatório')
+  })
+
+  it('feedback form validation', () => {
+    const data = pacient.invalidData
+    wrapper.setData({ pacient: data })
+    wrapper.vm.$forceUpdate()
+    const errors = wrapper.findAll('.error--message')
+    expect(errors.at(0).text()).toBe('Campo obrigatório')
+    expect(errors.at(1).text()).toBe('CPF inválido')
+    expect(errors.at(2).text()).toBe('Email inválido')
+    expect(errors.at(3).text()).toBe('Data inválida')
+  })
+
+  it('form should be valid', () => {
+    const data = pacient.validData
+    wrapper.setData({ pacient: data })
+    wrapper.vm.$forceUpdate()
+    expect(wrapper.findAll('.error--message').length).toBe(0)
+  })
+
+  it('PacientService post method should NOT be called on submit if form is INVALID', () => {
+    const data = pacient.invalidData
+    wrapper.setData({ pacient: data })
+    const serviceSpy = jest.spyOn(PacientService, 'post')
+    wrapper.vm.submit()
+    expect(serviceSpy).toBeCalledTimes(0)
+  })
+
+  it('PacientService post method should NOT be called on submit if form is VALID', () => {
+    const data = pacient.validData
+    wrapper.setData({ pacient: data })
+    const serviceSpy = jest.spyOn(PacientService, 'post')
+    wrapper.vm.submit()
+    expect(serviceSpy).toHaveBeenCalled()
   })
 })
